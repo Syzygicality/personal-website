e75a5d8eadf9b22ab120075eb8cdd77477ad0d63
Edison Wang <ew040607@gmail.com>
2025-11-17 01:36:23 -0500

    page config

--- app/Homepage.py
+++ app/Homepage.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Edison Wang",
+    page_icon="🔐",
+    layout="wide",
+)
+
 st.title("Edison Wang")
 st.divider()
 st.link_button("My LinkedIn", "https://www.linkedin.com/in/edison-wang-s7z7g7/")
