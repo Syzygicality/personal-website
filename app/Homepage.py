8819b4e9b3754fdba3b8f47c8c9fb0c55f01b93d
Edison Wang <ew040607@gmail.com>
2025-11-17 01:24:34 -0500

    github button

--- app/Homepage.py
+++ app/Homepage.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.title("Edison Wang")
 st.divider()
-st.link_button("My LinkedIn", "https://edisonwang.dev")
+st.link_button("My LinkedIn", "https://www.linkedin.com/in/edison-wang-s7z7g7/")
+st.link_button("My GitHub", "https://github.com/Syzygicality")
