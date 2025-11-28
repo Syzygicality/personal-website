b774ee14c096d5577b0f5285b07ed2468bcc990d
Edison Wang <ew040607@gmail.com>
2025-11-27 23:49:17 -0500

    setup tailwindcss

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Hello world!</p>
+<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="edison-wang-s7z7g7" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/edison-wang-s7z7g7?trk=profile-badge">Edison Wang</a></div>
